633847c852f162d1fd40e4d856f5c9326883e71f
Maksim Edush <maksim.edush@orangelogic.com>
2024-08-07 18:06:19 +0700

    update to 128 128

--- fasterdraw/server.js
+++ fasterdraw/server.js
@@ -9,6 +9,22 @@ const io = socketIo(server);
 
 // Connect to Redis
 const client = redis.createClient();
+const colors = {
+    0: [0, 0, 0], 1: [0, 0x55, 0],  2: [0,0xAA,0],  3: [0, 0xff, 0], 
+    4: [0, 0, 0xff], 5: [0,0x55,0xff],  6: [0, 0xaa, 0xff],  7: [0,0xff,0xff],
+    8: [0xff, 0, 0], 9: [0xff,0x55,0], 10: [0xff, 0xaa, 0], 11: [0xff, 0xff, 0], 
+   12: [0xff, 0, 0xff], 13:[0xff, 0x55, 0xff], 14: [0xff, 0xaa, 0xff], 15: [0xff,0xff,0xff]
+};
+const dimX=128;
+const dimY=128;
+
+(async () => {
+    // Connect to redis server
+    await client.connect();
+    client.del('img')
+    client.bitField("img",[{ operation: 'SET', encoding: 'u4', offset: '#'+(dimX*dimY-1), value: 0}])
+})();
+
 client.on('error', (err) => {
     console.error('Redis error:', err);
 });
@@ -16,23 +32,65 @@ client.on('error', (err) => {
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-app.get('/canvas.png', async (req, res) => {});
+app.get('/im', async (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    let uint8s = await readimg()
+    res.write(Buffer.from(uint8s));
+    console.log('b');
+    res.end(()=>{})
+});
+
+/// buffer: Buffer
+function splitBytes(buffer) {
+    const result = [];
+    for (let byte of buffer) {
+        const high4Bits = (byte >> 4) & 0x0F;
+        const low4Bits = byte & 0x0F;
+        result.push(high4Bits);
+        result.push(low4Bits);
+    }
+    return result;
+}
+
+function rgbToUint8ClampedArray(rgbs) {
+    let uint8s = new Uint8ClampedArray(dimX*dimY*4)
+    rgbs.forEach((a,i) => {    
+        uint8s[i*dimY+0] = colors[a][0];
+        uint8s[i*dimY+1] = colors[a][1];
+        uint8s[i*dimY+2] = colors[a][2];
+        uint8s[i*dimY+3] = 255;
+    });
+    return uint8s;
+}
+
+async function readimg() {
+    let data = await client.get('img')
+    if(!data){return;}
+    const buffer = Buffer.from(data, 'binary');
+    const result = splitBytes(buffer);
+    return result;
+}
+
+async function getUintClampedImage() {
+    let data = await client.get('img')
+    if(!data){return new Uint8ClampedArray(0)}
+    const buffer = Buffer.from(data, 'binary');
+    const result = splitBytes(buffer);
+
+    let uint8s = rgbToUint8ClampedArray(result);
+    return uint8s;
+}
 
 io.on('connection', async (socket) => {
     
-    if(!client.connected) {
-        await client.connect()
-    }
-
     console.log('A user connected');
+    
+    let d = await getUintClampedImage()
+    socket.emit('loadDrawing', d);
 
     socket.on('draw', (data) => {
-        client.bitField("img",[{
-            operation: 'SET',
-            encoding: 'u4',
-            offset: data.bit,
-            value: data.color
-        }])
+        client.bitField("img",[{ operation: 'SET', encoding: 'u4', offset: data.bit, value: data.color}])
+        socket.broadcast.emit("draw", data);
     });
 
     socket.on('disconnect', () => {
